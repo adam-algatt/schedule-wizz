24836167761b97ac6d18927a910becf9394cc9f4
adam-algatt <adam.algatt@gmail.com>
2022-07-22 14:00:33 -0700

    ready for final touch ups

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,74 +4,79 @@ let divIds = ['9', '10', '11', '12', '13', '14', '15', '16'];
 const divContainer = $('#hour-container');
 const scheduleContainer = $('#schedule');
 $('#date').text(moment().format('dddd, MMMM Do YYYY'));
+let count = 0;
 
-$(document).ready(() => {
-let currentTime = moment().format('H');
+function setTense() {
+  let tense;
+let currentTime = (moment().format('H'));
+
 divIds.forEach((id, index) => {
   let timeDifference =  Number(divIds[index]) - currentTime; 
-  let tense;
-  // move tense id set to separate function that can be called at intervals
-  //so that the colors correlate with time if app is left open
-  if (timeDifference > 0) {
+  
+    if (timeDifference > 0) {
     tense = "future"
   } else if (timeDifference < 0) {
     tense = "past"
   } else {
    tense = "present"
   }
-  console.log(tense);
-  
-  // console.log(id);
-  // if divIds[index] - currentTime < 0 
-  
-  scheduleContainer.append(`<div id=${tense} class="row col-12 border border-dark px-0">
-  <div id=${divIds[index]} class="col-2">
+if (count === 0) {
+  scheduleContainer.append(`<div id=${tense} class="row col-12 border border-dark px-0 rounded">
+  <div id="time-${divIds[index]}" class="col-2 text-center my-auto">
     ${divIds[index]}:00
   </div>
-  <div class="col-9 border-left border-right border-dark">
-    stuff
-
-  </div>
-    <button id="save-button" type="button " class="btn border-0 saveBtn hover-shadow mx-auto py-1 px-2">
+  <textarea id="notes-${divIds[index]}" class="col-9 border-left border-right border-dark p-1" >
+  </textarea>
+    <button id="${divIds[index]}" type="button" class="btn border-0 col-1 saveBtn hover-shadow mx-auto py-1 px-2">
       💾
     </button>
     </div>`)
-    $('.col-2').each(function () {
-   let time = parseInt(this.id, 10);
-  //  console.log(this);
-  //  if (time - currentTime < 0) {
-  // this.attr('time', 'past')  
-  // } else if (time - currentTime > 0) {
-  //   this.attr('time', 'future')  
-  //  } else {
-  //   this.attr('time', 'present') 
-  //  }
-    })
-  
+    $(`#notes-${divIds[index]}`).val(localStorage.getItem(`notes-${divIds[index]}`));
+}else{
+  $('.row').each(function(index, value){
+    let button = $(value).children('button');
+    let buttonId = $(button).attr('id');
+    let currentTense = this.id; 
+   let updatedTimeDifference = buttonId - currentTime;
+  //  console.log(buttonId, updatedTimeDifference, this.id);
+   if (updatedTimeDifference > 0 && currentTense !== "future") {
+      $(this).removeAttr('id');
+      $(this).attr('id', 'future'); 
+    } else if (updatedTimeDifference < 0 && currentTense !== "past") {
+      $(this).removeAttr('id');
+      $(this).attr('id', 'past'); 
+    } else if (updatedTimeDifference === 0 && currentTense !== "present") {
+      $(this).removeAttr('id');  
+      $(this).attr('id', 'present');  
+    } else {
+    }
+  })
+}
+
 })
+count++
+}
+
+// event listener to handle all child buttons of schedule container
+scheduleContainer.on('click', 'button', function(e) {
+  const siblingTextareaId = `notes-${this.id}`
+  console.log(e.target);
+  let textarea = $(e.target).siblings('textarea');
+ let note = $(textarea).val();
+  console.log(textarea, note);
+  localStorage.setItem(siblingTextareaId, note);
+//  turn above into var to grab textarea by 'id' and save .val to localStorage
+
+
 })
-// console.log(currentTime);
-//generate list items 
-// 8 hr workday 09:00, 10:00, 11:00, ... 13:00;
 
 
+// on document ready call set tense and add 30 second interval for calls
+$(document).ready(() => {
+setTense();
+// run setTense every half minute to color code divs
+setInterval(setTense, 30000 );
+})
 
 
-
-// var createTask = function (taskText, taskDate, taskList) {
-//     // create elements that make up a task item
-//     var taskLi = $("<li>").addClass("list-group-item");
-//     var taskSpan = $("<span>")
-//       .addClass("badge badge-primary badge-pill")
-//       .text(taskDate);
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(taskText);
-  
-//     // append span and p element to parent li
-//     taskLi.append(taskSpan, taskP);
-  
-//     // append to ul list on the page
-//     $("#list-" + taskList).append(taskLi);
-//   };
 
